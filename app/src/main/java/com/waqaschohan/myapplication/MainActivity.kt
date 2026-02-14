071e858eb2ff61a07720bb1375634454488b2db8
Waqas-Chohan <waqaschohan3355@gmail.com>
2026-02-14 11:23:19 +0500

    Performed Activity Lifecycle and navigate between activities

--- app/src/main/java/com/waqaschohan/myapplication/MainActivity.kt
+++ app/src/main/java/com/waqaschohan/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package com.waqaschohan.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "OnCreate")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,11 +21,60 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        if (savedInstanceState == null) {
+            Log.d("MainActivity", "MainFirstTime")
 
-        val button1: Button =findViewById(R.id.button1)
-        button1.setOnClickListener{
-            val intent = Intent(this,SecondActivity::class.java)
+        } else {
+            Log.d("MainActivity", "MainAccessedAgain")
+        }
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResumed")
+        Toast.makeText(this, "MainActivity:onResume", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStarted")
+        Toast.makeText(this, "MainActivity:onStart", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPaused")
+        Toast.makeText(this, "MainActivity:onPause", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStopped")
+        Toast.makeText(this, "MainActivity:onStop", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroyed")
+        Toast.makeText(this, "MainActivity:onDestroy", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestarted")
+        Toast.makeText(this, "MainActivity:onRestart", Toast.LENGTH_SHORT).show()
+
+    }
+
+
 }
--- app/src/main/java/com/waqaschohan/myapplication/SecondActivity.kt
+++ app/src/main/java/com/waqaschohan/myapplication/SecondActivity.kt
@@ -2,7 +2,9 @@ package com.waqaschohan.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("SecondActivity", "onCreate")
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,6 +21,56 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        if (savedInstanceState == null) {
+            Log.d("SecondActivity", "CreatedFirstTime")
+        } else {
+            Log.d("SecondActivity", "SecondAccessedAgain")
+        }
+
+
+        val button1: Button = findViewById(R.id.button2)
+        button1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SecondActivity", "onResumed")
+        Toast.makeText(this, "SecondActivity:onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SecondActivity", "onStarted")
+        Toast.makeText(this, "SecondActivity:onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SecondActivity", "onPaused")
+        Toast.makeText(this, "SecondActivity:onPause", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SecondActivity", "onStopped")
+        Toast.makeText(this, "SecondActivity:onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SecondActivity", "onDestroyed")
+        Toast.makeText(this, "SecondActivity:onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("SecondActivity", "onRestarted")
+        Toast.makeText(this, "SecondActivity:onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+
 }
